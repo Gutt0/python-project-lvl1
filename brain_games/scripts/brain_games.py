088434957284367e93c88116cdcfe6c55a52fecb
ilia <iliyachern@gmail.com>
2022-05-16 20:11:31 +0300

    added input name

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+from brain_games import cli
 
 def welcome():
     print('Welcome to the Brain Games!')
 
 def main():
     welcome()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
